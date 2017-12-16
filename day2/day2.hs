cc5f612c84a68782e43c98ef037a22fe1c0f8b63
Avi Caspe <avi.caspe@gmail.com>
2017-12-16 09:28:03 -0800

    Day 2 is now done.

---
+++ day2/day2.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import Data.Char
+
+parse :: String -> [[Int]]
+parse = map (map read . words) . lines
+
+part1 :: [[Int]] -> Int
+part1 = sum . (map lineDiff)
+      where lineDiff line = maximum line - minimum line
+
+part2 :: [[Int]] -> Int
+part2 = sum . (map lineVal)
+      where lineVal line = head [a `div` b | a <- line,  b <- line, a /= b, a `mod` b == 0]
+
+main :: IO ()
+main = do
+     input <- init <$> readFile "/home/avi/AoC2017/day2/input2"
+     let parsed = parse input
+     print $ part1 parsed
+     print $ part2 parsed
