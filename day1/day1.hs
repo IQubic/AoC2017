c3d90f263b0c0d0dad314f3df19a9e972f0f306e
Avi Caspe <avi.caspe@gmail.com>
2017-12-16 08:27:52 -0800

    Day 1 is now complete.

--- day1/day1.hs
+++ day1/day1.hs
@@ -8,12 +8,20 @@ parse = map digitToInt
 unpairs :: [(Int, Int)] -> [Int]
 unpairs pairs  = [a | (a, b) <- pairs, a == b]
 
+solve :: [(Int, Int)] -> Int
+solve = sum . unpairs
+
 part1 :: [Int] -> Int
-part1 x = sum $ unpairs pairs
+part1 x = solve pairs
       where pairs = zip x (drop 1 $ cycle x)
+
+part2 :: [Int] -> Int
+part2 x = solve pairs
+      where pairs = zip x (drop (length x `div` 2) $ cycle x)
 
 main :: IO ()
 main = do
-     input <- readFile "input1"
+     input <- init <$> readFile "/home/avi/AoC2017/day1/input1"
      let parsed = parse input
      print $ part1 parsed
+     print $ part2 parsed
